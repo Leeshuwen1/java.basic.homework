59fc2f99ff69608e0c345d38414547233cc8ddb3
Alexey Mulyukin <adiott52@gmail.com>
2024-01-30 18:16:52 +0300

    Исправил 2 и 5 задачу

--- src/main/java/ru/mulyukin/otus/java/basic/homework/homework3/Apply.java
+++ src/main/java/ru/mulyukin/otus/java/basic/homework/homework3/Apply.java
@@ -1,5 +1,7 @@
 package ru.mulyukin.otus.java.basic.homework.homework3;
 
+import java.sql.SQLOutput;
+
 public class Apply {
     public static void main(String[] args) {
 
@@ -39,60 +41,68 @@ public class Apply {
             for (int j = 0; j < arr[i].length; j++) {
                 if (i == 0 || j == 0 || i == arr.length - 1 || j == arr.length - 1) {
                     System.out.print("*");
+                } else {
+                    System.out.print(" ");
                 }
-                System.out.print(" ");
-            }
-            System.out.println();
-        }
-
-    }
-
-    public static int zeroingDiagonal(int[][] arr) {
-        int zero = 0;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                if (i == j || i + j == arr.length - 1) {
-                    arr[i][j] = zero;
-                    ;
-                }
-            }
-        }
-        return zero;
-    }
-
-    public static void print(int[][] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                System.out.print(arr[i][j] + " ");
             }
             System.out.println();
         }
     }
 
-    public static int findMax(int[][] arr) {
-        int max = 0;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                if (arr[i][j] > max) {
-                    max = arr[i][j];
+
+
+
+
+        public static int zeroingDiagonal ( int[][] arr){
+            int zero = 0;
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[i].length; j++) {
+                    if (i == j || i + j == arr.length - 1) {
+                        arr[i][j] = zero;
+
+                    }
                 }
             }
+            return zero;
         }
-        return max;
+
+        public static void print ( int[][] arr){
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[i].length; j++) {
+                    System.out.print(arr[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+//
+        public static int findMax ( int[][] arr){
+            int max = 0;
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[i].length; j++) {
+                    if (arr[i][j] > max) {
+                        max = arr[i][j];
+                    }
+                }
+            }
+            return max;
+        }
+
+        public static int sumSecondLine ( int[][] arr){
+            int sum = 0;
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[i].length; j++) {
+                    if (arr.length < 2) {
+                        return -1;
+                    }else if (i == 2){
+                        sum += arr[1][j];
+                    }
+                }
+            }
+            return sum;
+        }
     }
 
-    public static int sumSecondLine(int[][] arr) {
-        int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                if (i == 2) {
-                    sum += arr[2][j];
-                }
-            }
-        }
-        return sum;
-    }
-}
+
 
 
 
